82ab030152c96d5d699390f3b53214cb4c6cb355
Madeline Chin <madelinec98@nycstudents.net>
2024-09-28 00:14:17 -0400

    Created returnCopy function and wrote one test case.

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -9,8 +9,24 @@ public class MyArrays {
           }
           return end + "]";
       }
+	  
+	  public static int[] returnCopy(int[]ary) {
+		  int[] copy = new int[ary.length];
+		  for (int y = 0; y < ary.length; y++) {
+			  copy[y] = ary[y];
+		  }
+		  return copy;
+	  }
+	  
       public static void main(String[] args) {
+		//arrayToString
         int[] test = {1, 2, 3};
         System.out.println(arrayToString(test));
+		
+		//returnCopy
+		int[] test2 = {1, 2, 3};
+		System.out.println("Old: " + arrayToString(test2) 
+							+ " New: " + arrayToString(returnCopy(test2)));
+		System.out.println("Old address: " test2 + " New address: " + returnCopy(test2));
       }
 }
