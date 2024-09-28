f76d10e39ddedb3b05b292dc7a0ed721cc1c2ce3
Madeline Chin <madelinec98@nycstudents.net>
2024-09-28 00:33:08 -0400

    Created concatArray and wrote one test case for it.

--- 2024-09-27/MyArrays.java
+++ 2024-09-27/MyArrays.java
@@ -18,6 +18,19 @@ public class MyArrays {
 		  return copy;
 	  }
 	  
+	  public static int[] concatArray(int[]ary1,int[]ary2) {
+		  int[] tgt = int[ary1.length + ary2.length];
+		  for (int y = 0; y < ary1.length; y++) {
+			  tgt[y] = ary1[y];
+		  }
+		  for (int z = ary1.length; z < ary2.length + ary1.length; z++) {
+			  int i = 0;
+			  tgt[z] = ary2[i];
+			  i++;
+		  }
+		  return tgt;
+	  }
+	  
       public static void main(String[] args) {
 		//arrayToString
         int[] test = {1, 2, 3};
@@ -28,5 +41,11 @@ public class MyArrays {
 		System.out.println("Old: " + arrayToString(test2) 
 							+ " New: " + arrayToString(returnCopy(test2)));
 		System.out.println("Do the addresses match? " + (test2 == returnCopy(test2)));
+		
+		//concatArray
+		int[] test3 = {1, 2, 3};
+		int[] test4 = {4, 5, 6};
+		System.out.println("Add " + arrayToString(test3) + " to " + arrayToString(test4));
+		Sytem.out.println("Result: " arrayToString(concat(test3, test4)));
       }
 }
