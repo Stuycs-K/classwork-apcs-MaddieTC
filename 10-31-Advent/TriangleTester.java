a679c7ee5e3da69e9b58dc8558d5ea4319f61b71
Madeline Chin <madelinec98@nycstudents.net>
2024-11-04 10:57:40 -0500

    Tried to debug file but still does not run properly.

--- 10-31-Advent/TriangleTester.java
+++ 10-31-Advent/TriangleTester.java
@@ -6,19 +6,23 @@ public class TriangleTester {
     System.out.println(countTrianglesA("inputTri.txt"));
   }
   public static int countTrianglesA(String filename) {
+    //File file = new File(filename);
     Scanner input = new Scanner (filename);
     int count = 0;
-    String line = input.nextLine();
-    String[]nums = line.split(" ");
     while (input.hasNextLine()) {
+      String line = input.nextLine();
+      String[]nums = line.split(" ");
       for (int y = 0; y < nums.length; y++) {
-      if (validTri(Integer.parseInt(nums[y]), Integer.parseInt(nums[y] + 1), Integer.parseInt(nums[y]+2))) {
+      if (validTri(Integer.parseInt(nums[y]), Integer.parseInt(nums[y] + 1), Integer.parseInt(nums[y] + 2))) {
         count++;
       }
     }
   }
+  input.close();
+
   return count;
 }
+
   public static boolean validTri (int a, int b, int c) {
     return a + b > c && b + c > a && c + a > b;
   }
