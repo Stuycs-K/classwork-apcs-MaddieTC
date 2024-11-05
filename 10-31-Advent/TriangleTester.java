6442cc76d0fbba8feb3177448f27a2180f733be7
Madeline Chin <madelinec98@nycstudents.net>
2024-11-05 02:15:38 -0500

    First attempt at part B.

--- 10-31-Advent/TriangleTester.java
+++ 10-31-Advent/TriangleTester.java
@@ -3,7 +3,9 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 public class TriangleTester {
   public static void main(String[] args){
-    System.out.println(countTrianglesA("inputTri.txt"));
+   // System.out.println(countTrianglesA("inputTri.txt"));
+    System.out.println(countTrianglesB("inputTri.txt"));
+
   }
   public static int countTrianglesA(String filename) {
     int count = 0;
@@ -28,5 +30,35 @@ public class TriangleTester {
 
   public static boolean validTri (int a, int b, int c) {
     return a + b > c && b + c > a && c + a > b;
+  }
+  
+  public static int countTrianglesB(String filename) {
+	  int count = 0;
+	  try {
+		  File file = new File(filename);
+		  Scanner input = new Scanner (file);
+		  int[] uno = new int[1000];
+		  int[] dos = new int[1000];
+		  int[] tres = new int[1000];
+		  int i = 0;
+		  while (input.hasNextLine()) {
+			  String line = input.nextLine();
+			  String[]nums = line.split(" ");
+			  uno[i] = Integer.parseInt(nums[0]);
+			  dos[i] = (Integer.parseInt(nums[1]));
+			  tres[i] = (Integer.parseInt(nums[2]));
+			  i++;
+			  }
+		 for (int z = 0; z < i; z++) {
+			if (validTri(uno[z], dos[z], tres[z])) {
+				count++;
+		 }
+		 }
+	  }
+	  catch (FileNotFoundException ex) {
+      System.out.println("File not found");
+      return -1; 
+    }
+	  return count;
   }
 }
