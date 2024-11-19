94478de4e58eed96910b899972b1f41e68cdb216
Madeline Chin <madelinec98@nycstudents.net>
2024-11-19 10:58:26 -0500

    Attempt at Day 1 (compiles but does not work properly).

--- 2024-11-18-advent2016/Taxicab.java
+++ 2024-11-18-advent2016/Taxicab.java
@@ -1,32 +1,56 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 public class Taxicab {
   public static void main(String[] args) {
-	  
+    System.out.println(map("input.txt"));
   }
-  public static int map {
+  public static int map (String filename) {
 	  int xcoor = 0;
 	  int ycoor = 0;
 	  int dir = 0;
-	  File file = new File(filename);
+	  try {
+      File file = new File(filename);
 	  Scanner input = new Scanner (file);
 	  while (input.hasNext()) {
 		  String line = input.nextLine().trim();
 		  if (!line.isEmpty()) {
-			  String[]nums = line.split("\\s+");
-			  if (nums[0].indexOf("R") > -1) {
-				  dir++;
-				  if (dir == 4) {
-					  dir = 0;
-				  }
-				  xcoor = xcoor + Integer.parseInt(nums[0].charAt(1));
-			  }
-			  if (nums[0].indexOf("L") > -1) {
-				  dir--;
-				  if (dir == 4) {
-					  dir = 0;
-				  } 
-				  xcoor = xcoor - Integer.parseInt(nums[0].charAt(1));
-			  }
+			  String[]nums = line.split(", ");
+        for (int s = 0; s < nums.length; s++) {
+          if (nums[0].indexOf("R") > -1) {
+  				  dir++;
+  				  if (dir == 4) {
+  					  dir = 0;
+  				  }
+  			  }
+  			  if (nums[0].indexOf("L") > -1) {
+  				  dir--;
+  				  if (dir == -1) {
+  					  dir = 3;
+  				  }
+  			  }
+        if (dir == 0) {
+          ycoor = ycoor + Integer.parseInt(nums[0].substring(1));
+        }
+        if (dir == 1) {
+          xcoor = xcoor + Integer.parseInt(nums[0].substring(1));
+        }
+        if (dir == 2) {
+          ycoor = ycoor - Integer.parseInt(nums[0].substring(1));
+        }
+        if (dir == 3) {
+          xcoor = xcoor - Integer.parseInt(nums[0].substring(1));
+        }
+      }
+
+      }
 		  }
+      input.close();
 	  }
+    catch (FileNotFoundException ex) {
+      System.out.println("File not found");
+    }
+
+    return Math.abs(xcoor) + Math.abs(ycoor);
   }
 }
