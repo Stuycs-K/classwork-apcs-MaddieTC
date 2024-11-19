c4332aad7308fb36183f9056863493ab4bc6bdce
Madeline Chin <madelinec98@nycstudents.net>
2024-11-18 22:51:53 -0500

    Loose beginning framework for the code.

--- 2024-11-18-advent2016/Taxicab.java
+++ 2024-11-18-advent2016/Taxicab.java
@@ -1,5 +1,32 @@
 public class Taxicab {
   public static void main(String[] args) {
-
+	  
+  }
+  public static int map {
+	  int xcoor = 0;
+	  int ycoor = 0;
+	  int dir = 0;
+	  File file = new File(filename);
+	  Scanner input = new Scanner (file);
+	  while (input.hasNext()) {
+		  String line = input.nextLine().trim();
+		  if (!line.isEmpty()) {
+			  String[]nums = line.split("\\s+");
+			  if (nums[0].indexOf("R") > -1) {
+				  dir++;
+				  if (dir == 4) {
+					  dir = 0;
+				  }
+				  xcoor = xcoor + Integer.parseInt(nums[0].charAt(1));
+			  }
+			  if (nums[0].indexOf("L") > -1) {
+				  dir--;
+				  if (dir == 4) {
+					  dir = 0;
+				  } 
+				  xcoor = xcoor - Integer.parseInt(nums[0].charAt(1));
+			  }
+		  }
+	  }
   }
 }
