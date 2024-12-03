d006585e23c97fcf0d5b36c4c5786715adee5568
Madeline Chin <madelinec98@nycstudents.net>
2024-12-03 11:16:06 -0500

    Started my ColorDemo.

---
+++ 2024-12-03-colors/ColorDemo.java
@@ -0,0 +1,14 @@
+public class ColorDemo {
+  public static void main(String[] args) {
+    System.out.print("\u001b[47m");
+    System.out.println("                              ");
+    System.out.print("\u001b[0m");
+    System.out.print("\u001b[47m");
+    System.out.print("     ")
+    System.out.print("\u001b[40m");
+    System.out.print("     ");
+    System.out.print("\u001b")
+    System.out.print("\u001b")
+    System.out.print("\u001b")
+  }
+}
