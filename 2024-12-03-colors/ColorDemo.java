b7ac8d196266b9aa782cd37460ded82fc9c5bbe4
Madeline Chin <madelinec98@nycstudents.net>
2024-12-04 01:05:00 -0500

    Completed a masterpiece.

--- 2024-12-03-colors/ColorDemo.java
+++ 2024-12-03-colors/ColorDemo.java
@@ -1,14 +1,96 @@
 public class ColorDemo {
   public static void main(String[] args) {
-    System.out.print("\u001b[47m");
-    System.out.println("                              ");
+	System.out.print("\u001b[47m");
+    System.out.println("\n");
+	
+    System.out.print("                                          ");
+	System.out.print("\u001b[48;2;128;128;128m");
+    System.out.print("             ");
+    System.out.println("\u001b[47m");
+	
+    System.out.print("                                        ");
+	System.out.print("\u001b[48;2;128;128;128m");
+    System.out.print("                 ");
+    System.out.println("\u001b[47m");
+	
+	System.out.print("                                      ");
+	System.out.print("\u001b[48;2;128;128;128m");
+    System.out.print("                     ");
+    System.out.println("\u001b[47m");
+
+	System.out.print("                                      ");
+	System.out.print("\u001b[48;2;128;128;128m");
+    System.out.print("                     ");
+    System.out.println("\u001b[47m");
+	
+	System.out.print("                                      ");
+	System.out.print("\u001b[48;2;128;128;128m");
+    System.out.print("                     ");
+    System.out.println("\u001b[47m");
+	
+	System.out.print("                                      ");
+	System.out.print("\u001b[48;2;128;128;128m");
+    System.out.print("                     ");
+    System.out.println("\u001b[47m");
+	
+	System.out.print("                                      ");
+	System.out.print("\u001b[48;2;128;128;128m");
+    System.out.print("                     ");
+    System.out.println("\u001b[47m");
+	
+	
+
+    System.out.print("                                    ");
+	System.out.print("\u001b[48;2;128;128;128m");
+    System.out.print("                      ");
+    System.out.println("\u001b[47m");
+	
+    System.out.print("                                 ");
+	System.out.print("\u001b[48;2;128;128;128m");
+    System.out.print("                      ");
+    System.out.println("\u001b[47m");
+
+    System.out.print("                               ");
+	System.out.print("\u001b[48;2;128;128;128m");
+    System.out.print("     O                ");
+    System.out.println("\u001b[47m");
+
+    System.out.print("                             ");
+	System.out.print("\u001b[48;2;128;128;128m");
+    System.out.print("                      ");
+    System.out.println("\u001b[47m");
+
+
+    System.out.print("                          ");
+	System.out.print("\u001b[48;2;255;209;223m");
+    System.out.print("  ");
+	System.out.print("\u001b[48;2;128;128;128m");
+    System.out.print("                     ");
+    System.out.println("\u001b[47m");
+	
+    System.out.print("                              ");
+	System.out.print("\u001b[48;2;128;128;128m");
+    System.out.print("                ");
+    System.out.println("\u001b[47m");
+    System.out.println("\n");
+    System.out.print("                                      ");
+	System.out.print("\u001b[1m");
+	System.out.print("\u001b[4m");
+	System.out.print("\u001b[38;2;128;0;128m");
+	System.out.print("\u001b[48;2;200;0;0m");
+	System.out.print("RAT");
+    System.out.println("\u001b[47m");
+
+	
+
+
+
+
+
+	
+
     System.out.print("\u001b[0m");
-    System.out.print("\u001b[47m");
-    System.out.print("     ")
-    System.out.print("\u001b[40m");
-    System.out.print("     ");
-    System.out.print("\u001b")
-    System.out.print("\u001b")
-    System.out.print("\u001b")
+   // System.out.print("\u001b")
+   // System.out.print("\u001b")
   }
 }
