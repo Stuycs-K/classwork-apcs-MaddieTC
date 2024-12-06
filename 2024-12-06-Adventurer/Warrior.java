2eb5a35d8006a5130c6287ca0a4a2f3e8cf95a7c
Madeline Chin <madelinec98@nycstudents.net>
2024-12-06 10:33:24 -0500

    Added abstract class and started my child class.

---
+++ 2024-12-06-Adventurer/Warrior.java
@@ -0,0 +1,26 @@
+public class Warrior extends Adventurer {
+  private String name;
+  private int HP,maxHP;
+  
+  public abstract String getSpecialName();
+  //accessor methods
+  public abstract int getSpecial();
+  public abstract void setSpecial(int n);
+  public abstract int getSpecialMax();
+
+  /*
+    all adventurers must have a way to attack enemies and
+    support their allys
+  */
+  //hurt or hinder the target adventurer
+  public abstract String attack(Adventurer other);
+
+  //heall or buff the target adventurer
+  public abstract String support(Adventurer other);
+
+  //heall or buff self
+  public abstract String support();
+
+  //hurt or hinder the target adventurer, consume some special resource
+  public abstract String specialAttack(Adventurer other);
+}
