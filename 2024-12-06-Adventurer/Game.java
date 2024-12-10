bef14d0384628bacd8a675fd1df31b879bc0969a
Madeline Chin <madelinec98@nycstudents.net>
2024-12-09 22:07:24 -0500

    Added opponent moves, fixed formatting errors, and made sure the game continued until quit.

--- 2024-12-06-Adventurer/Game.java
+++ 2024-12-06-Adventurer/Game.java
@@ -5,7 +5,7 @@ class Game {
     Scanner userInput = new Scanner(System.in);
 
     //You can do the rest many times:
-
+while (true) {
     System.out.println("Enter username");
     //Read one line of user input
     String userName = userInput.nextLine();
@@ -22,7 +22,12 @@ class Game {
     while (a.getHP() > 0 && b.getHP() > 0) {
 		String input = userInput.nextLine();
     if (input.equals("a") || input.equals("attack")) {
-      a.attack(b);
+      System.out.println(a.attack(b));
+	  if (b.getHP() > 0) {
+	  System.out.println();
+	  System.out.println(Game.back(b,a));
+	  System.out.println();
+	  }
       System.out.println(userName + ", " + a.getHP() + "/20 HP, " +
       a.getSpecial() + "/100 Strength");
       System.out.println("Goose, " + b.getHP() + "/20 HP, " +
@@ -31,7 +36,12 @@ class Game {
 	  System.out.println();
     }
 	else if (input.equals("sp") || input.equals("special")) {
-      a.specialAttack(b);
+      System.out.println(a.specialAttack(b));
+	  if (b.getHP() > 0) {
+	  System.out.println();
+	  System.out.println(Game.back(b,a));
+	  System.out.println();
+	  }
       System.out.println(userName + ", " + a.getHP() + "/20 HP, " +
       a.getSpecial() + "/100 Strength");
       System.out.println("Goose, " + b.getHP() + "/20 HP, " +
@@ -40,7 +50,12 @@ class Game {
 	  System.out.println();
     }
 	else if (input.equals("su") || input.equals("special")) {
-      a.support();
+      System.out.println(a.support());
+	  if (b.getHP() > 0) {
+	  System.out.println();
+	  System.out.println(Game.back(b,a));
+	  System.out.println();
+	  }
       System.out.println(userName + ", " + a.getHP() + "/20 HP, " +
       a.getSpecial() + "/100 Strength");
       System.out.println("Goose, " + b.getHP() + "/20 HP, " +
@@ -56,5 +71,27 @@ class Game {
 		System.out.println();
 	}
 }
+	if (a.getHP() <= 0) {
+	System.out.println("Game over! You lost :(");
+	System.out.println();
+	}
+	else if (b.getHP() <= 0) {
+		System.out.println("Game over! Goose lost :)");
+		System.out.println();
+	}
+}
+  }
+  private static String back(Adventurer one, Adventurer other) {
+	  int ran = (int) (Math.random() * 3) + 1;
+	  if (ran == 1) {
+		  return one.attack(other);
+	  }
+	  else if (ran == 2) {
+		  return one.specialAttack(other);
+	  }
+	  else if (ran == 3) {
+		  return one.support();
+	  }
+	  return "Something went wrong :(";
   }
 }
